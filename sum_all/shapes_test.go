69d3c163e5debf626955248cb285d63ff7dc04e1
Chris Toynbee <ctoynbee@gmail.com>
2021-10-06 09:59:09 +1300

    shape methods

--- sum_all/shapes_test.go
+++ sum_all/shapes_test.go
@@ -16,7 +16,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
@@ -26,7 +26,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 
 		if got != want {
