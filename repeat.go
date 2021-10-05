85bc032a8ae0fe80562e8bdf7c955efa2e2fa1a1
Chris Toynbee <ctoynbee@gmail.com>
2021-10-05 15:46:34 +1300

    more test stuff

---
+++ repeat.go
@@ -0,0 +1,9 @@
+package iteration
+
+func Repeat(character string, repeatedCount int) string {
+	var repeated string
+	for i := 0; i < repeatedCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
