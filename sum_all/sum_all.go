275130ace6bae6759f12839572a7869b83752ad2
Chris Toynbee <ctoynbee@gmail.com>
2021-10-06 09:33:47 +1300

    Shapes

--- sum_all/sum_all.go
+++ sum_all/sum_all.go
@@ -1,12 +1,40 @@
 package main
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+// 	sums := make([]int, lengthOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+
+// 	return sums
+// }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
 }
+
+func SumAllHeads(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	headsTotals := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		headNumber := []int{numbers[0]}
+		headsTotals[i] = Sum(headNumber)
+	}
+	return headsTotals
+}
+
+// len(numbers) == 0
+// empty?(numbers)
